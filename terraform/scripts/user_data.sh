e077f4f4fd7157d122eb7860716b35b2229d4a2e
manojmac07 <manojmac77@gmail.com>
2025-06-09 15:07:29 +0530

    update of daigrams

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -4,7 +4,7 @@ apt install -y python3-pip git
 pip3 install apache-airflow==2.6.0 dbt-core==1.5.0 apache-airflow-providers-snowflake==3.3.0 dbt-snowflake==1.5.0
 mkdir -p /home/ubuntu/{dags,dbt_project}
 cd /home/ubuntu
-git clone https://github.com/your-org/your-repo.git project
+git clone git@github.com:manojmac07/datapipeline.git project
 
 ln -s project/dags dags
 ln -s project/dbt_project dbt_project
